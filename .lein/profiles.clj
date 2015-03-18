4118f8c68be32bed5670949c46e64ac5317e6c26
Vincent Rivellino <vince79@gmail.com>
2015-03-18 15:27:10 -0400

    lein-ancient update

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[lein-ring "0.8.11"]
                   [lein-midje "3.1.3"]
                   [lein-pprint "1.1.1"]
-                  [lein-ancient "0.6.0"]]}}
+                  [lein-ancient "0.6.5"]]}}
