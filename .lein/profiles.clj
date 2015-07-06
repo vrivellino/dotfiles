b338971ce9354f941ec26bb69623b73ee5b41d46
Vincent Rivellino <vincent.rivellino@stelligent.com>
2015-07-06 16:24:43 -0400

    Add lein-kibit to profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-midje "3.1.3"]
                   [lein-pprint "1.1.2"]
-                  [lein-ancient "0.6.7"]]}}
+                  [lein-ancient "0.6.7"]
+                  [lein-kibit "0.1.2"]]}}
