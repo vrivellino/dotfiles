f2dc0891f9ea46fc25d51d83db8263fa88dc8c1f
Vincent Rivellino <vrivellino@demystdata.com>
2015-11-30 13:49:13 -0500

    Install JRuby thru RVM

--- setup/common/20-rvm.sh
+++ setup/common/20-rvm.sh
@@ -8,3 +8,14 @@ if [ ! -d ~/.rvm ]; then
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   \curl -sSL https://get.rvm.io | bash -s stable --ruby --ignore-dotfiles
 fi
+
+if ! rvm list | grep jruby-9.0.3.0 > /dev/null; then
+  rvm install jruby-9.0.3.0
+  rvm use jruby-9.0.3.0
+  gem install bundle
+fi
+if ! rvm list | grep jruby-9.0.4.0 > /dev/null; then
+  rvm install jruby-9.0.4.0
+  rvm use jruby-9.0.4.0
+  gem install bundle
+fi
