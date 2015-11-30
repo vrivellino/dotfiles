30e526898d60d80a5d0763625ea1324c135017b6
Vincent Rivellino <vrivellino@demystdata.com>
2015-11-30 10:18:00 -0500

    Use openssl from homebrew
    El Capitan doesn't ship openssl headers, so use it from homebrew.

--- setup/osx/10-brew.sh
+++ setup/osx/10-brew.sh
@@ -10,3 +10,9 @@ fi
 
 brew install automake bash casperjs coreutils git gnu-getopt gnu-sed gnu-tar gpg maven node packer pkg-config qt5 tidy-html5 tree vim
 brew install czmq libffi libyaml pyqt python
+
+# El Capitan (and presumably higher) does not ship openssl headers
+darwinversion=$(echo $OSTYPE | sed 's/^darwin//')
+if [ $darwinversion -ge 15 ]; then
+    brew link openssl --force
+fi
