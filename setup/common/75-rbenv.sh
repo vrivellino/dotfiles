f656353e6998c31df6d83ddd16aeda410a7fc1aa
Vincent Rivellino <vrivellino@demystdata.com>
2015-12-15 10:19:13 -0500

    rbenv setup tweaks
    * only install in dev environments
    * make 2.2.3 the default

--- setup/common/75-rbenv.sh
+++ setup/common/75-rbenv.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 set -ex
 
+# only install rbenv if we're setting up a dev environment
+[ "$dev_install" = 'true' ] || exit 0
+
 cd
 
 if [ ! -h .rbenv ] && [ -e .rbenv ]; then
   mv .rbenv .rbenv.pre-vr
 fi
-if pushd ~/.dotfiles/rbenv/plugins > /dev/null; then
+
+if pushd ~/.dotfiles/rbenv > /dev/null; then
+  [ -d plugins ] || mkdir plugins
+  cd plugins
   rm -rf ruby-build
   ln -snf ../../ruby-build
 
   popd > /dev/null
   ln -snf ~/.dotfiles/rbenv .rbenv
+
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  type rbenv | command grep -q -F 'rbenv is a function' || eval "$(rbenv init -)"
+
+  rbenv install 2.2.3
+  rbenv global 2.2.3
+
+  gem install bundler
+  gem install rake
 fi
