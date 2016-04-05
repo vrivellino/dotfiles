vrivellino/dotfiles

commit aa7e53031e15f11a555a91a5d2348de28bea2753

Don't set LC_ALL shell-wide

bashrc.d/20-aliases.bash
@@ -2,9 +2,8 @@
 if [ "$(uname -s)" = 'Darwin' ]; then
   CLICOLOR=1
   LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
-  LC_ALL=POSIX
-  export CLICOLOR LSCOLORS LC_ALL
-  alias ls='ls -F -T -b -a'
+  export CLICOLOR LSCOLORS
+  alias ls='LC_ALL=POSIX ls -F -T -b -a'
 
   # gnu tools I need on my mac
   gsed_path="$(which gsed 2>&1)"

commit 68c6dc76b33f2df4aa7bf0b766dc936ec1fdc699

Fix randpass() (broke it in aa7e530)

bashrc.d/20-aliases.bash
@@ -27,6 +27,8 @@ alias ipycon="mkdir -p ~/.ipython/ && ipython qtconsole > ~/.ipython/con.out 2>&
 
 # random password generator
 randpass() {
+    local lc_all_old="$LC_ALL"
+    export LC_ALL=POSIX
     < /dev/urandom tr -dc '_.!@#$%^&*A-Z-a-z-0-9' | head -c${1:-10};echo;
     < /dev/urandom tr -dc '_.!@#$%^&*A-Z-a-z-0-9' | head -c${1:-12};echo;
     < /dev/urandom tr -dc '_.!@#$%^&*A-Z-a-z-0-9' | head -c${1:-16};echo;
@@ -34,4 +36,5 @@ randpass() {
     < /dev/urandom tr -dc A-Z-a-z-0-9 | head -c${1:-10};echo;
     < /dev/urandom tr -dc A-Z-a-z-0-9 | head -c${1:-12};echo;
     < /dev/urandom tr -dc A-Z-a-z-0-9 | head -c${1:-16};echo;
+    export LC_ALL="$lc_all_old"
 }