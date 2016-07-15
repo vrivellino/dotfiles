93adb78eb3627c3c0e8e03dc127f3c4b381df6a2
Vincent Rivellino <vrivellino@demystdata.com>
2016-07-15 11:32:26 -0400

    Comment ipython qtconsole alias for now
    vim-ipython doesn't work with latest versions of IPython, so I'm just
    using ipython on the terminal & vim-slime

--- bashrc.d/20-aliases.bash
+++ bashrc.d/20-aliases.bash
@@ -23,7 +23,7 @@ alias vi=vim
 alias grep='grep --color'
 alias zgrep='zgrep --color'
 alias fgrep='fgrep --color'
-alias ipycon="mkdir -p ~/.ipython/ && ipython qtconsole > ~/.ipython/con.out 2>&1 &"
+#alias ipycon="mkdir -p ~/.ipython/ && ipython qtconsole > ~/.ipython/con.out 2>&1 &"
 
 # random password generator
 randpass() {
