773b53f3f73c17a9eafd0cebc71530de587b5d5b
Vincent Rivellino <vrivellino@demystdata.com>
2017-05-15 15:24:32 -0400

    Bump ruby verions

--- setup/common/75-rbenv.sh
+++ setup/common/75-rbenv.sh
@@ -22,8 +22,9 @@ if pushd ~/.dotfiles/rbenv > /dev/null; then
   export PATH="$HOME/.rbenv/bin:$PATH"
   type rbenv | command grep -q -F 'rbenv is a function' || eval "$(rbenv init -)"
 
-  rbenv install 2.2.5
-  rbenv global 2.2.5
+  rbenv install 2.4.1
+  rbenv install 2.2.7
+  rbenv global 2.4.1
 
   gem install rake
   gem install bundler
