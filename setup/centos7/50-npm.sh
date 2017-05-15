f2f993f447e85fda13382fef1ab3bf43c695d649
Vincent Rivellino <vrivellino@demystdata.com>
2017-05-15 15:20:54 -0400

    Work around /usr/local/bin not in sudo path

--- setup/centos7/50-npm.sh
+++ setup/centos7/50-npm.sh
@@ -4,7 +4,8 @@ set -ex
 # only install node tools if we're setting up a dev environment
 [ "$dev_install" = 'true' ] || exit 0
 
-sudo /usr/local/bin/npm install -g gulp
-sudo /usr/local/bin/npm install -g jslint
-sudo /usr/local/bin/npm install -g jsonlint
-sudo /usr/local/bin/npm install -g jshint
+node_cmd="$(which $node)"
+sudo "$node_cmd" /usr/local/bin/npm install -g gulp
+sudo "$node_cmd" /usr/local/bin/npm install -g jslint
+sudo "$node_cmd" /usr/local/bin/npm install -g jsonlint
+sudo "$node_cmd" /usr/local/bin/npm install -g jshint
