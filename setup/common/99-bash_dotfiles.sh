3e6ba8c4e839f53e2cbecad9694ce8348311dc9a
Vincent Rivellino <vrivellino@demystdata.com>
2018-01-11 14:36:37 -0500

    Quiet greps

--- setup/common/99-bash_dotfiles.sh
+++ setup/common/99-bash_dotfiles.sh
@@ -6,10 +6,10 @@ cd
 
 ln -snf .dotfiles/.bashrc .bashrc_vr
 
-if ! grep -F 'source ~/.bashrc_vr' ~/.bashrc; then
+if ! grep -q -F 'source ~/.bashrc_vr' ~/.bashrc; then
   echo '[ -f ~/.bashrc_vr ] && source ~/.bashrc_vr' >> ~/.bashrc
 fi
 
-if ! grep -F 'source ~/.bashrc' ~/.bash_profile; then
+if ! grep -q -F 'source ~/.bashrc' ~/.bash_profile; then
   echo '[ -f ~/.bashrc ] && source ~/.bashrc' >> ~/.bash_profile
 fi
