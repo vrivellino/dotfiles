67169815f6d49cd6d342f4d7af5f28c7a9148943
Vincent Rivellino <vrivellino@demystdata.com>
2018-04-01 17:53:47 -0400

    Add rubocop config

--- setup/common/90-misc-config.sh
+++ setup/common/90-misc-config.sh
@@ -7,13 +7,17 @@ test -d .config || mkdir -m 0700 .config
 if [ ! -h .config/flake8 ] && [ -e .config/flake8 ]; then
     mv .config/flake8 .config/flake8.pre-vr
 fi
-
 ln -snf ~/.dotfiles/config-flake8 .config/flake8
 
 if [ ! -h .config/yamllint ] && [ -e .config/yamllint ]; then
     mv .config/yamllint .config/yamllint.pre-vr
 fi
 ln -snf ~/.dotfiles/config-yamllint .config/yamllint
+
+if [ ! -h .rubocop.yml ] && [ -e .rubocop.yml ]; then
+    mv .rubocop.yml .rubocop.yml.pre-vr
+fi
+ln -snf ~/.dotfiles/.rubocop.yml .rubocop.yml
 
 # finish vim
 if [[ $OSTYPE =~ darwin ]]; then
